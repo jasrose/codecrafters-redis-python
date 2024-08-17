000e06aa06acadfa4a1d5c919080d644941de3e6
Jasmine Rose <jasmine.raquel.rose@gmail.com>
2024-08-17 19:05:16 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import socket
 
 
+
 def main():
     print("Logs from your program will appear here!")
 
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept() # wait for client
-    client.send(b"+PONG\r\n")
+    
+
+    while True:
+        request: bytes = client.recv(512)
+        data: str = request.decode()
+
+        if "ping" in data.lower():
+            client.send("+PONG\r\n".encode())
+
 
 
 if __name__ == "__main__":
