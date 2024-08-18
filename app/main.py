64449c290882098dc4e108b15b61799a27e1c489
Jasmine Rose <jasmine.raquel.rose@gmail.com>
2024-08-18 00:58:52 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 
+database = {}
+
 def main():
     print("Starting server...")
 
@@ -48,7 +50,19 @@ def determine_response(command: str) -> str:
     elif len(parts) == 5 and parts[2] == "ECHO":
         message = parts[4]
         return build_echo_response(message)
+    
+    # Check if the command is SET
+    elif len(parts) == 4 and parts[2] == "SET":
+        key = parts[3]
+        value = parts[4]
+        store_key_value(key, value)
+        return "+OK\r\n"
+    elif len(parts) == 4 and parts[2] == "GET":
+        key = parts[3]
+        value = parts[4]
+        get_key_value(key, value)
 
+    
     # Default response for unrecognized commands
     return "-ERR unknown command\r\n"
 
@@ -58,6 +72,15 @@ def build_echo_response(message: str) -> str:
 
     # Construct the RESP bulk string response with only the message
     return f"${message_length}\r\n{message}\r\n"
+
+
+def store_key_value(key_value: str, value: str) -> None:
+    database[key_value] = value
+
+def get_key_value(key_value: str, value: str):
+    database[key_value] = value
+    value_length = len(value)
+    return f"${value_length}\r\n{value}\r\n"
 
 if __name__ == "__main__":
     main()
